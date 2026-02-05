7ad26c48ff34ca19e4e5156e923a002977f66752
BrenC11 <brendancleaves@me.com>
2026-02-05 16:33:03 +0000

    Remove prerender plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import path from 'path';
 import { defineConfig } from 'vite';
-import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 
 export default defineConfig({
-  plugins: [
-    vitePrerenderPlugin({
-      renderTarget: '#root',
-      prerenderScript: path.resolve(__dirname, 'prerender.tsx'),
-    }),
-  ],
+  plugins: [],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '.'),
